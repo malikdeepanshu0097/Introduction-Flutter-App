d90833238b53a2a87b0883576ff7e59a24731fb6
Deepanshu Malik <malikdeepanshu0097@gmail.com>
2020-09-22 22:52:02 +0530

    First Flutter App

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/photo.JPG'),
+              ),
+              Text(
+                "Deepanshu Malik",
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 60,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0,
+                    color: Colors.white),
+              ),
+              Text(
+                "Flutter Developer",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0,
+                    color: Colors.white),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  title: Text(
+                    '+91 8700021118',
+                    style: TextStyle(),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  title: Text(
+                    'malikdeepanshu0097@gmail.com',
+                    style: TextStyle(),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
